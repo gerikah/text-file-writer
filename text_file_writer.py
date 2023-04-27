2fb6414253a005caebf39df2d004c8ed1ff4110b
gerikah <aldaygerikah@gmail.com>
2023-04-28 00:24:52 +0800

    pseudocode

---
+++ text_file_writer.py
@@ -0,0 +1,10 @@
+# open the file in write mode
+# iterate until the user is done entering line
+    # prompt the user to enter a line of text
+    # read user input
+    # write the line to the file
+    # prompt the user to enter if there are more lines to enter
+    # read user input
+    # if user input  is "y", continue
+    # if user input is "n", break the loop
+# close the file
