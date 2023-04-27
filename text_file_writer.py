437e133afc8a0c7b16b654d4a692d6932b89084b
gerikah <aldaygerikah@gmail.com>
2023-04-28 01:13:17 +0800

    loop, get user's input line, and write to file

--- text_file_writer.py
+++ text_file_writer.py
@@ -5,12 +5,12 @@ def write_to_file():
     # open the file in write mode
     with open("mylife.txt", "w") as my_file:
         # iterate until the user is done entering line
-        
-        # prompt the user to enter a line of text
-
-    # read user input
-    # write the line to the file
-    # prompt the user to enter if there are more lines to enter
+        while True:
+            # prompt the user to enter a line of text
+            line = input("enter line: ")
+            # write the line to the file
+            my_file.write (line + "\n")
+            # prompt the user to enter if there are more lines to enter
     # read user input
     # if user input  is "y", continue
     # if user input is "n", break the loop
