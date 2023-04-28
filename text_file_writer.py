a3f20c0a63b69f2dd619bfc9a5890fc38496ae52
gerikah <aldaygerikah@gmail.com>
2023-04-28 09:54:31 +0800

    prompt to enter more line

--- text_file_writer.py
+++ text_file_writer.py
@@ -1,17 +1,17 @@
 
-name = input("what is your name? ")
+name = input ("what is your name? ")
 
 def write_to_file():
     # open the file in write mode
-    with open("mylife.txt", "w") as my_file:
+    with open ("mylife.txt", "w") as my_file:
         # iterate until the user is done entering line
         while True:
             # prompt the user to enter a line of text
-            line = input("enter line: ")
+            line = input ("enter line: ")
             # write the line to the file
             my_file.write (line + "\n")
             # prompt the user to enter if there are more lines to enter
-    # read user input
-    # if user input  is "y", continue
-    # if user input is "n", break the loop
+            new_line = input (f"are there more lines, {name}? y/n ")
+            # if user input  is "y", continue
+            # if user input is "n", break the loop
 # close the file
