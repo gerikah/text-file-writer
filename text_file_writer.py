30d2ceae99af4c2f389c6d697a210a4e5910aaf0
gerikah <aldaygerikah@gmail.com>
2023-04-28 18:41:17 +0800

    import pyfiglet

--- text_file_writer.py
+++ text_file_writer.py
@@ -1,5 +1,14 @@
+# Object Oriented Programming CMPE-103 PROGRAMMING EXERCISE: Problem 3
+# ALDAY, Gerikah L. - BSCPE 1-5
 
-name = input("what is your name? ")
+import pyfiglet
+border = "-" * 180
+title = ("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format("text file\nwriter\n", justify = "center", font = "isometric1", width = 175) + "\n")
+print(title)
+
+name = ("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format("what is your name?\n", justify = "center", font = "cybermedium", width = 175) + "\n")
+print(name)
+input_name = input(" ".center(80))
 
 def write_to_file():
     # open the file in write mode
@@ -8,19 +17,24 @@ def write_to_file():
         # iterate until the user is done entering line        
         while True:
 
-            # prompt the user to enter a line of text
-            line = input("enter new line: ")
-
             border = "-" * 180
-            # write the line to the file
-            my_file.write(f"{border}\n\n{line}\n\n")
+            
+            # prompt the user to enter a line of text
+            line = ("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format("enter new line\n", justify = "center", font = "cybermedium", width = 175) + "\n")
+            print(line)
+            input_line = input(" ".center(80))
 
             
+            # write the line to the file
+            my_file.write(f"{border}\n\n{input_line}\n\n")
+
             # prompt the user to enter if there are more lines to enter
-            new_line = input (f"are there more line, {name}? y/n: ")
+            new_line = ("\n\n" + border + "\n\n" + "\033[95m" + pyfiglet.figlet_format("are there more line,\n" + input_name + "? y/n" , justify = "center", font = "cybermedium", width = 175) + "\n")
+            print(new_line)
+            decision = input (" ".center(80))
             
             # if user input  is "y", continue
-            if new_line.lower() == "y":
+            if decision.lower() == "y":
                 continue
             
             # if user input is "n", break the loop
